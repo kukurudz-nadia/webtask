901a91c438a6360b448f07043f69003095cf4956
Kukurudz Nadiia <94442918+kukurudz-nadia@users.noreply.github.com>
2023-03-06 22:36:31 +0200

    news
     news

--- news.js
+++ news.js
@@ -1,17 +1,13 @@
 function displayData() {
-  // Get the input values
   let input1 = document.getElementById("input1").value;
   let input2 = document.getElementById("input2").value;
   let input3 = document.getElementById("input3").files[0];
 
-  // Create a new paragraph element to display the data
   let outputDiv = document.getElementById("output");
-  outputDiv.innerHTML = ""; // clear any existing content
+  outputDiv.innerHTML = "";
   let outputP = document.createElement("p");
 
-  // Add the text inputs to the paragraph
   outputP.textContent = input1 + input2;
-  // If an image was selected, add it to the paragraph
   if (input3) {
     let outputImg = document.createElement("img");
     outputImg.src = URL.createObjectURL(input3);
@@ -19,6 +15,9 @@ function displayData() {
     outputDiv.appendChild(outputImg);
   }
 
-  // Add the paragraph to the output div
   outputDiv.appendChild(outputP);
+
+  document.getElementById("input1").value = "";
+  document.getElementById("input2").value = "";
+  document.getElementById("input3").value = "";
 }
